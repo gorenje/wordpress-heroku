4b32c47b93daa6c954267ec51575fd6f5be6bd63
Jo <jo@Jos-iMac.local>
2013-09-04 17:43:23 +0200

    Activated <details> support for Firefox

--- wp-content/themes/mywooga/footer.php
+++ wp-content/themes/mywooga/footer.php
@@ -8,5 +8,12 @@
       <a href="<?php echo wp_logout_url( $redirect ); ?>" rel="logout">Logout</a>
     </div>
   </footer>
+  <script src="http://code.jquery.com/jquery-2.0.3.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="<?php echo get_bloginfo( 'template_directory' ) ?>/js/jquery.details.min.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript" charset="utf-8">
+    jQuery(function(){
+      jQuery('details').details();
+    })
+  </script>
 </body>
 </html>
