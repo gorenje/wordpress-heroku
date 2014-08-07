32880ed6bbaa63b21f40cbed3bba361dacecaab6
ignacio <ignacio.revuelta@wooga.com>
2014-08-07 16:05:40 +0200

    changed filepaths

--- wp-content/themes/mywooga/header.php
+++ wp-content/themes/mywooga/header.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" type="text/css" />
-  <link rel="stylesheet" href="http://static.wooga.com/wp-content/themes/wooga-dev/fonts/grilled-cheese.css" type="text/css" />
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700" type="text/css" />
+  <link rel="stylesheet" href="//static.wooga.com/wp-content/themes/wooga-dev/fonts/grilled-cheese.css" type="text/css" />
   <link rel="stylesheet" href="<? echo get_stylesheet_uri(); ?>" type="text/css" />
   <!-- Wordpress Header Code -->
   <?php wp_head(); ?>
